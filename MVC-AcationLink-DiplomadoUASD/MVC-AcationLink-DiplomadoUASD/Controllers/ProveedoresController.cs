834f5e28be7bbff09ce10a7d3df69743d036c585
Ronald-PN <45144094+Ronald-PN@users.noreply.github.com>
2019-03-05 22:58:42 -0400

    Initial commit

---
+++ MVC-AcationLink-DiplomadoUASD/MVC-AcationLink-DiplomadoUASD/Controllers/ProveedoresController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_AcationLink_DiplomadoUASD.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        // GET: Proveedores
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        public ActionResult Listado()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC-AcationLink-DiplomadoUASD/MVC-AcationLink-DiplomadoUASD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_AcationLink_DiplomadoUASD.MvcApplication" Language="C#" %>
